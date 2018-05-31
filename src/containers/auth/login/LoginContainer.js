6f3418887706e2e85bf0cbd5def6c6f11dac70f2
sandeep mt <mtsandeep@gmail.com>
2018-05-31 11:41:17 +0530

    added bulma sass version and basic style for login

--- src/containers/auth/login/LoginContainer.js
+++ src/containers/auth/login/LoginContainer.js
@@ -3,18 +3,23 @@ import { SITE_NAME } from 'constants';
 import styles from './LoginContainer.scss';
 
 const Login = () => (
-  <div className={styles.wrapper}>
-    <h2>Login to {SITE_NAME}</h2>
-    <form>
-      <div className={styles.field}>
-        <label>Username/Email</label>
-        <input type="text" />
-      </div>
-      <div className={styles.field}>
-        <label>Password</label>
-        <input type="password" />
-      </div>
-    </form>
+  <div className={styles.container}>
+    <h2 className={styles.title}>Login to {SITE_NAME}</h2>
+    <div className={styles.wrapper}>
+      <form>
+        <div className={styles.field}>
+          <label>Username/Email</label>
+          <input type="text" />
+        </div>
+        <div className={styles.field}>
+          <label>Password</label>
+          <input type="password" />
+        </div>
+        <div className={styles.field}>
+          <input className={styles.submitBtn} type="submit" value="LOGIN" />
+        </div>
+      </form>
+    </div>
   </div>
 );
 
